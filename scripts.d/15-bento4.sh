3da137657ffb5901e698979b297ed94c0b9b1df6
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2023-08-20 08:02:32 +0300

    Update 15-bento4.sh

--- scripts.d/15-bento4.sh
+++ scripts.d/15-bento4.sh
@@ -11,11 +11,10 @@ ffbuild_enabled() {
 ffbuild_dockerbuild() {
     cd "$FFBUILD_DLDIR/$SELF"
 
-    mkdir build && cd build
-
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" ..
 
-    make
+    make -j$(nproc)
+    make install
 }
 
 ffbuild_configure() {
